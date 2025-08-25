5e0bf097ce208be98c407429c600852ce564d0c7
LiviuCaciulatu <liviucaciulatu@gmail.com>
2025-08-25 20:51:54 +0300

    project setup

---
+++ asociatialimfedem/app/components/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import styles from "./HomePage.module.scss";
+
+export default function HomePage() {
+  return (
+    <main className={styles.homePage}>
+      {/* Home page content goes here */}
+    </main>
+  );
+}
---
+++ asociatialimfedem/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+
+const nextConfig: NextConfig = {
+  i18n: {
+    locales: ["ro"],
+    defaultLocale: "ro",
+  },
+  /* config options here */
+};
+
+export default nextConfig;
